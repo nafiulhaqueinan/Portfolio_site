d9c7e536350838c8b23eb02e6c9c9f9d539c860b
nafiulhaqueinan <nafiulhaqueinan01@gmail.com>
2024-10-14 19:18:48 +0600

    add desktop navigation system
    here i am add home services resume work contact in my site in navigation part

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,30 @@
 import Link  from "next/link"
 import { Button } from "./ui/button"
 
+//components
+import Nav from "./Nav";
 
 const Header=()=>{
     return(
         <header className="py-8 xl:py-12 text-white">
-            <div className="container mx-auto">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* {logos} */}
                 <Link href={"/"}>
                 <h1 className="text-4xl font-semibold">
                     Nafiul<span className="text-accent">.</span>
-
                 </h1>
                 </Link>
-                {/* {desktop nav} */}
-                <Nav />
+
+                {/* {desktop nav ans hire me button} */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href="/contact">
+                      <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                {/* mobile Nav */}
+                <div className="xl:hidden">movile nav</div>
             </div>
         </header>
     )
