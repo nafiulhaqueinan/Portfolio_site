a191853cb50f25dcbb3e3e91e4560d79c87e17c6
nafiulhaqueinan <nafiulhaqueinan01@gmail.com>
2024-10-14 16:33:43 +0600

    nav and header update

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,12 @@ module.exports = {
     container: {
       center: true,
       padding: "15px",
-      screens: {
+    },
+    screens: {
         sm : "640px",
         md : "768px",
         lg : "960px",
         xl : "1200px",
-        
-      },
     },
     fontFamily: {
       primary: "var(--font-jetbrainsMOno)",
@@ -28,7 +27,7 @@ module.exports = {
       colors:  {
         primary: '#1c1c22',
         accent:{
-          default: '#00ff99',
+          DEFAULT: '#00ff99',
           hover: '#00e187',
         }
       },
