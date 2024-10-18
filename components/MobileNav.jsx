7b20952aa884bd9c2bb57385d58f02dfed1914c4
nafiulhaqueinan <nafiulhaqueinan01@gmail.com>
2024-10-19 01:49:41 +0600

    mobile nav done and work

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -12,25 +12,25 @@ const links =[
     },
     {
         name : 'services',
-        path : '/',
+        path : '/services',
 
     },
     {
         name : 'resume',
-        path : '/',
+        path : '/resume',
 
     },
     {
         name : 'work',
-        path : '/',
+        path : '/work',
 
     },
     {
         name : 'contact',
-        path : '/',
+        path : '/contact',
 
     },
- ] 
+ ];
 
 
 const MobileNav=()=>{
@@ -43,7 +43,28 @@ const MobileNav=()=>{
             </SheetTrigger>
             <SheetContent className="flex flex-col">
             {/*logo*/}
-            <div>logo</div>
+            <div className="mt-32 mb-40 text-center text-2xl">
+                <Link href="/">
+                <h1 className="text-4xl font-semibold">
+                Nafiul<span className='text-accent'>.</span>    
+                </h1>
+                </Link>
+            </div>
+            <nav className="flex flex-col justify-center items-center gap-8">
+            {links.map((link,index)=>{
+                return (
+                    <Link
+                    href={link.path}
+                    key={index}
+                    className={`${
+                        link.path === pathname && "text-accent border-b-2 border-accent"
+                    } text-xl capitalize hover:text-accent transition-all`}
+                    >
+                     {link.name}
+                    </Link>
+                );
+            })}
+            </nav>
             </SheetContent>
         </Sheet>
     );
