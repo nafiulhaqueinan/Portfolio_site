724932122969d42d5190d939ca7ec2c8abf1fa34
nafiulhaqueinan <nafiulhaqueinan01@gmail.com>
2024-10-20 19:33:10 +0600

    change button and home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,29 @@
+import { Button } from "@/components/ui/button";
+import{FiDownload} from "react-icons/fi";
 
-export default function Home() {
-  return <main>homepage</main>;
-}
+const Home = () =>{
+  return(
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          
+          {/*text*/}
+          <div className="text-center xl:text-left">
+            <span>Software Developer</span>
+          </div>
+
+
+          {/*photo*/}
+          <div>photo</div>
+
+
+        </div>
+      </div>
+    </section>
+  );
+};
+
+
+export default Home;
+
+
