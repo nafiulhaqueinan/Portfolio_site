971acfb2ee4bcfb1a0d52f76e2f0630b17bb3365
nafiulhaqueinan <nafiulhaqueinan01@gmail.com>
2024-10-23 22:57:01 +0600

    img section done

--- components/ui/Photo.jsx
+++ components/ui/Photo.jsx
@@ -5,32 +5,55 @@ import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="w-full h-full relative">
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
-        }}
-      >
+    <div className="w-full h-full flex items-center justify-center relative">
+      {/* Wrapping div to keep things centered */}
+      <div className="relative w-[298px] h-[298px] xl:w-[490px] xl:h-[490px]">
+        {/* Image */}
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[400px] xl:h-[400px] mix-blend-light"
+          className="absolute top-0 left-0 w-full h-full"
         >
           <Image
-            src="/assets/3.png"
+            src="/assets/33.png"
             priority
             quality={100}
             fill
-            alt=""
+            alt="Image"
             className="object-contain"
           />
         </motion.div>
-      </motion.div>
+        {/* Circle */}
+        <motion.svg
+          className="absolute top-0 left-0 w-full h-full"
+          fill="transparent"
+          viewBox="0 0 506 506"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <motion.circle
+            cx="253"
+            cy="253"
+            r="250"
+            stroke="#00ff99"
+            strokeWidth="4"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+              rotate: [120, 360],
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        </motion.svg>
+      </div>
     </div>
   );
 };
