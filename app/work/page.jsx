c731eb30d098a2ec33c4b2a3d6728ed48a57fe82
nafiulhaqueinan <nafiulhaqueinan01@gmail.com>
2024-10-27 23:20:02 +0600

    444

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,3 +1,18 @@
+"use client";
+
+import { motion } from "framer-motion";
+import React,{useState} from "react";
+
+import{Swiper,SwiperSlide}from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight,BsGithub } from "react-icons/bs";
+import { Tooltip,TooltipContent,TooltipProvider,TooltipTrigger } from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+
 const work = () =>{
     return (
         <div> work page </div>
